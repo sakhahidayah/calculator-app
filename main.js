ba7afc0d3ff0f0695a7e0233022a57896a63127f
sakhahidayah <sakhahidayahramadhan@gmail.com>
2025-05-31 20:46:43 +0700

    dark mode and light mode calculator app

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let result = document.getElementById("result");
+let historyCalculation = document.getElementById("history");
 let calculation = [];
-const buttons = document.querySelectorAll("#btn");
+let histroryResult = [];
+const buttons = document.querySelectorAll(".btn");
 const operator = document.querySelectorAll("#calculation");
 const results = document.getElementById("results");
 const deletes = document.getElementById("delete");
@@ -24,16 +26,54 @@ results.addEventListener("click", () => {
   let resultsValues = calculation.join("");
   let finalResult = eval(resultsValues);
   result.textContent = finalResult;
+
+  histroryResult = resultsValues;
+  historyCalculation.textContent = histroryResult;
 });
 
 deletes.addEventListener("click", () => {
   let calculationValues = calculation;
-  deleteArray();
-  function deleteArray() {
-    for (let i = 0; i < calculationValues.length; i++) {
-      calculationValues.splice(i);
+  histroryResult = "";
+  historyCalculation.textContent = "";
+  deleteArray(calculationValues);
+
+  function deleteArray(index) {
+    for (let i = 0; i < index.length; i++) {
+      index.splice(i);
     }
   }
+
   calculationValues.push(calculation);
   result.textContent = calculation;
+});
+
+// dark mode
+
+const lightMode = document.getElementById("lightMode");
+const darkMode = document.getElementById("darkMode");
+const darkToggle = document.getElementById("darkToggle");
+const lightToggle = document.getElementById("lightToggle");
+const container = document.getElementById("container");
+
+
+darkToggle.addEventListener("click", () => {
+  lightMode.classList.remove("hidden");
+  darkMode.classList.add("hidden");
+  container.classList.remove("bg-white");
+  container.classList.add("bg-[#151434]", "text-white");
+  buttons.forEach((button) => {
+    button.classList.remove("bg-[rgba(226,226,226,255)]");
+    button.classList.add("bg-[#2d2b46]");
+  });
+});
+
+lightToggle.addEventListener("click", () => {
+  lightMode.classList.add("hidden");
+  darkMode.classList.remove("hidden");
+  container.classList.remove("bg-[#151434]", "text-white");
+  container.classList.add("bg-white");
+  buttons.forEach((button) => {
+    button.classList.remove("bg-[#2d2b46]");
+    button.classList.add("bg-[rgba(226,226,226,255)]");
+  });
 });
