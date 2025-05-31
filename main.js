f3aa79d65b4715cc5588e0da987cfd59aeb9f865
sakhahidayah <sakhahidayahramadhan@gmail.com>
2025-05-31 10:26:05 +0700

    update operator + delete

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-const result = document.getElementById("result");
+let result = document.getElementById("result");
 let calculation = [];
 const buttons = document.querySelectorAll("#btn");
 const operator = document.querySelectorAll("#calculation");
 const results = document.getElementById("results");
-const deletes = document.getElementById('delete')
+const deletes = document.getElementById("delete");
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    const value = button.value;
+    let value = button.value;
     calculation.push(value);
     result.textContent = calculation.join("");
   });
@@ -14,16 +14,26 @@ buttons.forEach((button) => {
 
 operator.forEach((operators) => {
   operators.addEventListener("click", () => {
-    const values = operators.value;
+    let values = operators.value;
     calculation.push(values);
     result.textContent = calculation.join("");
   });
 });
 
 results.addEventListener("click", () => {
-  result.value = eval();
+  let resultsValues = calculation.join("");
+  let finalResult = eval(resultsValues);
+  result.textContent = finalResult;
 });
 
-deletes.addEventListener('click',() => {
-  
-})
+deletes.addEventListener("click", () => {
+  let calculationValues = calculation;
+  deleteArray();
+  function deleteArray() {
+    for (let i = 0; i < calculationValues.length; i++) {
+      calculationValues.splice(i);
+    }
+  }
+  calculationValues.push(calculation);
+  result.textContent = calculation;
+});
